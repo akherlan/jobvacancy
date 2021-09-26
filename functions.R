9e3593e52c10c2057460baa87772dafa2a0c8e30
akherlan <herlandi.27@gmail.com>
2021-09-26 07:55:03 +0700

    add function for detect R skill in job requirement

--- functions.R
+++ functions.R
@@ -1,6 +1,6 @@
 library(jsonlite)
 library(stringr)
-library(dplyr)
+suppressPackageStartupMessages(library(dplyr))
 
 # get vacancy
 glints_vacancy <- function(limit = 50L, cty = "ID", type = "FULL_TIME") {
@@ -34,19 +34,31 @@ glints_vacancy <- function(limit = 50L, cty = "ID", type = "FULL_TIME") {
     "%22CountryCode%22:%22", countryCode, "%22%2C",
     "%22type%22:%22", jobTypes, "%22%2C",
     "%22JobCategoryId%22:", JobCategoryId, "%2C",
-    # '"startDate":"', startDate, '%22%2C',
+    # '%22startDate%22:%22', startDate, '%22%2C',
     "%22status%22:%22", status, "%22}&limit=", limit
   )
   
+  
   vacancy <- fromJSON(query)
   
-  if(vacancy$count > 0) {
-    print(
+  if(vacancy$count > 0){
+    message(
       paste("Mengambil", limit, "dari", vacancy$count, "data lowongan kerja", 
             sep = " ")
     )
+  } else if (vacancy$count == 0){
+    # if there is no data in the startDate, stop the process
+    stop(
+      paste0(
+        "\nTidak ada data lowongan pada ", startDate,
+        "\nProses dihentikan"
+      )
+    )
   } else {
-    print(paste0("Tidak ada lowongan pada ", startDate))
+    # another condition
+    stop(
+      cat("Terjadi kesalahan")
+    )
   }
   
   vacancy <- vacancy$data %>% 
@@ -54,27 +66,52 @@ glints_vacancy <- function(limit = 50L, cty = "ID", type = "FULL_TIME") {
     select(id, title, JobCategoryId, CompanyId, type, descriptionRaw, 
            minYearsOfExperience, maxYearsOfExperience, numberOfHires,
            deadline, startDate, endDate, durationLegacy, expiryDate,
-           status, isPublic, isRemote, closedAt, createdAt, updatedAt)
+           status, isPublic, isRemote, closedAt, createdAt, updatedAt) %>% 
+    arrange(desc(updatedAt))
   
   return(vacancy)
   
 }
 
+# get job url
+glints_joburl <- function(vacancy, num){
+  
+  joburl <- paste(
+    "https://glints.com/id/opportunities/jobs",
+    vacancy$title[[num]] %>% 
+      tolower() %>% 
+      str_remove_all("\\(") %>% 
+      str_remove_all("\\)") %>% 
+      str_remove_all("\\[") %>% 
+      str_remove_all("\\]") %>% 
+      str_remove_all("-") %>% 
+      str_remove_all("\\.") %>% 
+      str_remove_all("\\/") %>% 
+      str_remove_all("&") %>% 
+      str_squish() %>% 
+      str_replace_all("\\s", "-"), 
+    vacancy$id[[num]],
+    sep = "/")
+  
+  return(joburl)
+  
+}
+
 
 # post formatting
-glints_post <- function(rownum = 1L) {
+glints_post <- function(vacancy, num = 1L) {
   
   # using generic function: glints_descform()
   
   # position
   
-  jobtitle <- vacancy$title[[rownum]]
+  jobtitle <- vacancy$title[[num]]
   
   # company
   # link: https://glints.com/id/companies/{CompanyName}/{CompanyId}
   
   jobhire_baseurl <- "https://glints.com/api/companies/"
-  company_id <- vacancy$CompanyId[[rownum]]
+  company_id <- vacancy$CompanyId[[num]]
   
   jobhire <- fromJSON(
     paste0(jobhire_baseurl, company_id)
@@ -82,24 +119,11 @@ glints_post <- function(rownum = 1L) {
   
   # desctiption
   
-  jobdesc <- glints_descform(vacancy, rownum)
+  jobdesc <- glints_descform(vacancy, num)
   
   # link
   
-  joburl <- paste(
-    "https://glints.com/id/opportunities/jobs",
-    jobtitle %>% 
-      tolower() %>% 
-      str_remove_all("\\(") %>% 
-      str_remove_all("\\)") %>% 
-      str_remove_all("-") %>% 
-      str_remove_all("\\.") %>% 
-      str_remove_all("\\/") %>% 
-      str_remove_all("&") %>% 
-      str_squish() %>% 
-      str_replace_all("\\s", "-"), 
-    vacancy$id[[rownum]],
-    sep = "/")
+  joburl <- glints_joburl(vacancy, num)
   
   # body
   
@@ -156,20 +180,19 @@ glints_company <- function(limit = 50L, cty = "ID") {
 
 
 # description formatting
-glints_descform <- function(df, rownum) {
+glints_descform <- function(df, num) {
   
   # one row data to be formatted
-  topost_data <- df[rownum,] %>% 
-    select(id, title, CompanyId, isRemote, descriptionRaw,
-           minYearsOfExperience, maxYearsOfExperience,
-           expiryDate, createdAt, updatedAt)
-  
-  topost_desc <- topost_data %>% 
-    select(descriptionRaw)
-  
+  if(sum(str_detect(names(df), "descriptionRaw")) == 1){
+    topost_desc <- df[num,] %>% 
+      select(descriptionRaw)
+  } else {
+    stop("Kolom deskripsi tidak tersedia")
+  }
+
   # descprition tabulation
   desc <- topost_desc$descriptionRaw$blocks[[1]]
-  desc <- bind_cols(desc$text, desc$type, desc$depth)
+  suppressMessages(desc <- bind_cols(desc$text, desc$type, desc$depth))
   names(desc) <- c("text", "type", "depth")
   
   # html formatting markup
@@ -179,16 +202,17 @@ glints_descform <- function(df, rownum) {
     # unstyled list with manual bullet or numering
     topost_text <- desc %>% 
       mutate(
-        text = str_remove_all(text, "\\n"),
+        text = str_replace_all(text, "\\n", "<br>"),
         text = str_replace(text, "^\\d{1,2}\\.?\\s?", "• "),
         text = str_replace(text, "^-\\s?", "• "),
         text = str_replace(text, "^·\\s?", "• "),
+        text = str_replace_all(text, "<br>-\\s", "<br>• "),
         text = ifelse(
           str_count(text, "([A-Za-z]+\\s)") <= 2 & 
             !str_detect(text, "• "),
           paste0("<br><strong>", text, "</strong><br>"),
           text)
-        ) %>%  
+      ) %>%  
       .[[1]] %>% 
       toString() %>% 
       str_replace_all(",\\s•\\s", "<br>• ") %>% 
@@ -204,7 +228,7 @@ glints_descform <- function(df, rownum) {
     # styled indent list
     topost_text <- desc %>% 
       mutate(
-        text = str_remove_all(text, "\\n"),
+        text = str_replace_all(text, "\\n", "<br>"),
         text = ifelse(
           str_detect(type, "list-item"),
           paste0("• ", text),
@@ -235,3 +259,22 @@ glints_descform <- function(df, rownum) {
   return(topost_text)
   
 }
+
+# detect R skill requirement in text description
+detect_r <- function(text){
+  if(
+    sum(str_detect(text, "\\sr$")) > 0 |
+    sum(str_detect(text, "\\sr\\.")) > 0 |
+    sum(str_detect(text, "\\sr\\s")) > 0 |
+    sum(str_detect(text, "^r\\s")) > 0 |
+    sum(str_detect(text, "\\sr,")) > 0 |
+    sum(str_detect(text, ",r,")) > 0 |
+    sum(str_detect(text, ",r\\s")) > 0 |
+    sum(str_detect(text, "\\sr\\-\\d")) > 0 
+  ){
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
