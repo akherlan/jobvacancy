d505377b844e2fea77ef96c2c0d4fd42da4e4a9c
Andi <getdata@duck.com>
2023-01-25 11:25:33 +0700

    Fix #2, temporary #3

--- audit.R
+++ audit.R
@@ -22,8 +22,8 @@ send_msg <- function(bot, msg) {
           parse_mode = "Markdown"
         )
         Sys.sleep(2)
-      }, 
-      error = function(e){
+      },
+      error = function(e) {
         cat("ERROR:", conditionMessage(e), "\n")
       }
     )
@@ -36,22 +36,28 @@ scraped <- read.csv("output/audit.csv")
 # Jobstreet
 # pulling data
 cat("Get data from Jobstreet...\n")
-j <- tryCatch({ suppressMessages(map_df(key_search, ~jobstreet(.x))) }, 
-              error = function(e) {
-                cat("ERROR: ", conditionMessage(e), "\n")
-              })
+j <- tryCatch({
+  suppressMessages(map_df(key_search, ~jobstreet(.x)))
+},
+error = function(e) {
+  cat("ERROR: ", conditionMessage(e), "\n")
+})
 j <- unique(j)
-j <- map_df(location, ~{ j[grepl(.x, j$city),] })
+j <- map_df(location, ~{
+  j[grepl(.x, j$city), ]
+})
 
 # filtering with previous scraped data
-j <- j[!(j$job_id %in% scraped$id),]
+j <- j[!(j$job_id %in% scraped$id), ]
 
 # filtering title
-j_title <- map(exclude_title, ~{ grepl(.x, tolower(j$job_title)) })
+j_title <- map(exclude_title, ~{
+  grepl(.x, tolower(j$job_title))
+})
 j_title <- data.frame(j_title[[1]], j_title[[2]])
 j_title[[(ncol(j_title) + 1)]] <- as.logical(rowSums(j_title))
 j_title <- !j_title[[ncol(j_title)]]
-j <- j[j_title,]
+j <- j[j_title, ]
 
 cat("Reading description info...\n")
 j_desc <- map_chr(j$job_url, ~try(jobdescform(.x)))
@@ -60,16 +66,22 @@ j_keys <- map(keyword, ~grepl(.x, j_desc))
 j_keys <- data.frame(j_keys[[1]], j_keys[[2]])
 j_keys[[(ncol(j_keys) + 1)]] <- as.logical(rowSums(j_keys))
 j_keys <- j_keys[[ncol(j_keys)]]
-j <- j[j_keys,]
+j <- j[j_keys, ]
 
-if (nrow(j) == 0) { cat("No job from Jobstreet to post today\n") } else {
-  cat(paste0("Total ", nrow(j), " job(s). Keywords:\n"))
-  print(map_chr(keyword, ~.x))
+if (nrow(j) == 0) {
+  cat("No job from Jobstreet to post today\n")
+} else {
+  cat(
+    paste0(
+      "Total ", nrow(j), " job(s). Keywords: ",
+      paste0(map_chr(keyword, ~.x), collapse = ", ")
+    )
+  )
   # create message
-  msg <- map(1:nrow(j), ~{
+  msg <- map(seq_len(nrow(j)), ~{
     paste0(
       "*", toupper(j$job_title[.x]), "*\nat ", j$company[.x], "\n\n",
-      j$job_url[.x], 
+      j$job_url[.x],
       "\n\nCategory: ", j$category,
       "\nKeyword: #", keyword[[1]], " #", keyword[[2]]
     )
@@ -78,8 +90,8 @@ if (nrow(j) == 0) { cat("No job from Jobstreet to post today\n") } else {
   cat("Sending message to Telegram...\n")
   send_msg(bot, msg)
   # save scraped details
-  j_scrape <- j[c("job_title", "company", 'source', "job_id")]
-  names(j_scrape) <- c("job_title", "company_name", 'source', "id")
+  j_scrape <- j[c("job_title", "company", "source", "job_id")]
+  names(j_scrape) <- c("job_title", "company_name", "source", "id")
   j_scrape$timestamp <- as.character(Sys.time())
   j_scrape$timezone <- Sys.timezone()
   scraped <- rbind(scraped, j_scrape)
@@ -88,22 +100,28 @@ if (nrow(j) == 0) { cat("No job from Jobstreet to post today\n") } else {
 # Glints
 # pulling data
 cat("Get data from Glints...\n")
-g <- tryCatch({ suppressMessages(map_df(key_search, ~glints(.x))) }, 
-              error = function(e) {
-                cat("ERROR: ", conditionMessage(e), "\n")
-              })
+g <- tryCatch({
+  suppressMessages(map_df(key_search, ~glints(.x)))
+},
+error = function(e) {
+  cat("ERROR: ", conditionMessage(e), "\n")
+})
 g <- unique(g)
-g <- map_df(location, ~{ g[grepl(.x, g$city),] })
+g <- map_df(location, ~{
+  g[grepl(.x, g$city), ]
+})
 
 # filtering with previous scraped data
-g <- g[!(g$job_id %in% scraped$id),]
+g <- g[!(g$job_id %in% scraped$id), ]
 
 # filtering title
-g_title <- map(exclude_title, ~{ grepl(.x, tolower(g$job_title)) })
+g_title <- map(exclude_title, ~{
+  grepl(.x, tolower(g$job_title))
+})
 g_title <- data.frame(g_title[[1]], g_title[[2]])
 g_title[[(ncol(g_title) + 1)]] <- as.logical(rowSums(g_title))
 g_title <- !g_title[[ncol(g_title)]]
-g <- g[g_title,]
+g <- g[g_title, ]
 
 cat("Reading description info...\n")
 g_desc <- map_chr(g$job_url, ~try(glints_desc(.x)))
@@ -112,16 +130,22 @@ g_keys <- map(keyword, ~grepl(.x, g_desc))
 g_keys <- data.frame(g_keys[[1]], g_keys[[2]])
 g_keys[[(ncol(g_keys) + 1)]] <- as.logical(rowSums(g_keys))
 g_keys <- g_keys[[ncol(g_keys)]]
-g <- g[g_keys,]
+g <- g[g_keys, ]
 
-if (nrow(g) == 0) { cat("No job from Glints to post today\n") } else {
-  cat(paste0("Total ", nrow(g), " job(s). Keywords:\n"))
-  print(map_chr(keyword, ~.x))
+if (nrow(g) == 0) {
+  cat("No job from Glints to post today\n")
+} else {
+  cat(
+    paste0(
+      "Total ", nrow(g), " job(s). Keywords: ",
+      paste0(map_chr(keyword, ~.x), collapse = ", ")
+    )
+  )
   # create message
-  msg <- map(1:nrow(g), ~{
+  msg <- map(seq_len(nrow(g)), ~{
     paste0(
       "*", toupper(g$job_title[.x]), "*\nat ", g$company[.x], "\n\n",
-      g$job_url[.x], 
+      g$job_url[.x],
       "\n\nCategory: ", g$category[.x],
       "\nKeyword: #", keyword[[1]], " #", keyword[[2]]
     )
@@ -130,8 +154,8 @@ if (nrow(g) == 0) { cat("No job from Glints to post today\n") } else {
   cat("Sending message to Telegram...\n")
   send_msg(bot, msg)
   # save scraped details
-  g_scrape <- g[c("job_title", "company", 'source', "job_id")]
-  names(g_scrape) <- c("job_title", "company_name", 'source', "id")
+  g_scrape <- g[c("job_title", "company", "source", "job_id")]
+  names(g_scrape) <- c("job_title", "company_name", "source", "id")
   g_scrape$timestamp <- as.character(Sys.time())
   g_scrape$timezone <- Sys.timezone()
   scraped <- rbind(scraped, g_scrape)
--- function/jstreet_descform.R
+++ function/jstreet_descform.R
@@ -13,85 +13,88 @@ jstreet_descform <- function(df, num) {
     # get html
     if (is.null(df$job_url[n])) url <- get_joburl(id = df$id[n])
     else url <- df$job_url[n]
-    f <- read_html(url)
-    
-    # assume prefered description tag
-    desc <- html_element(f, ".YCeva_0") # sebelumnya .vDEj0_0
-    desc <- desc %>% 
-      html_children() %>% 
-      html_children() %>% 
-      html_children()
-    
-    # chech indent
-    if (sum(html_name(desc) == "div") == length(desc)) {
-      desc <- html_children(desc)
-    } else if (sum(html_name(desc) == "ul") > 0 | sum(html_name(desc) == "ol") > 0) {
-      desc <- desc
+    # f <- read_html(url)
+    f <- tryCatch(
+      {
+        read_html(url)
+      },
+      error = function(e) {
+        message(paste("[ERROR]", url, conditionMessage(e)))
+      }
+    ) # end of try
+    if (is.null(f)) {
+      topost_text <- "Replacement text"
+      if (n == num[[1]]) txt <- topost_text
+      else txt <- append(txt, topost_text)
+      next
     } else {
-      desc <- desc
+      # assume prefered description tag
+      desc <- html_element(f, "._1x1c7ng0") # sebelumnya .vDEj0_0, .YCeva_0
+      desc <- desc %>% 
+        html_children() %>% 
+        html_children() %>% 
+        html_children()
+      # chech indent
+      if (sum(html_name(desc) == "div") == length(desc)) {
+        desc <- html_children(desc)
+      } else if (sum(html_name(desc) == "ul") > 0 | sum(html_name(desc) == "ol") > 0) {
+        desc <- desc
+      } else {
+        desc <- desc
+      }
+      for (node in seq_along(desc)) {
+        if (html_name(desc[node]) == "ul")
+          d <- html_children(desc[node])
+        else if (html_name(desc[node]) == "ol")
+          d <- html_children(desc[node])
+        else
+          d <- desc[node]
+        if (node == 1)
+          descrip <- d
+        else
+          descrip <- append(descrip, d)
+      }
+      attributes(descrip)$class <- attributes(desc)$class
+      # description tabulation
+      tag <- sapply(descrip, html_name)
+      content <- sapply(descrip, html_text)
+      desc <- tibble(tag, content)
+      # format text
+      desc <- desc %>% 
+        mutate(content = str_squish(content),
+               content = ifelse(str_detect(tag, "li"), 
+                                str_replace(content, "^(.+)$", "• \\1"),
+                                content),
+               content = ifelse((tag == "div" | tag == "p") & 
+                                  str_count(content, "\\w+\\s") < 4 &
+                                  !str_detect(content, "^-\\s") &
+                                  !str_detect(content, "•\\s"),
+                                str_replace(content, "^(.+)$", "<strong>\\1</strong>"),
+                                content),
+               content = ifelse((tag == "div" | tag == "p") & nchar(content) < 50,
+                                str_replace(content, "^(.+)$", "<strong>\\1</strong>"),
+                                content),
+               content = ifelse(tag == "strong",
+                                str_replace(content, "^(.+)$", "<strong>\\1</strong>"),
+                                content),
+               content = str_replace(content, "^(.+)$", "\\1<br>"),
+               content = str_replace(content, "^<strong>", "<br><strong>"))
+      # filter long paragraph
+      desc <- desc %>% 
+        filter(!((tag == "div" | tag == "p") & nchar(content) > 200))
+      # clean format
+      topost_text <- desc$content %>% 
+        toString() %>% 
+        str_replace_all(",\\s,\\s", "<br>") %>% 
+        str_replace_all(">,\\s", ">") %>% 
+        str_replace_all("<br>-\\s", "<br>• ") %>% 
+        str_replace_all("(\\w+\\.?\\s?)<br>(\\w+)", "\\1<br><br>\\2") %>% 
+        str_replace_all("<br></strong>(<br>)?", "</strong><br><br>") %>% 
+        str_remove("^<br>") %>% 
+        str_remove("<br>$")
+      if (n == num[[1]]) txt <- topost_text
+      else txt <- append(txt, topost_text)
     }
-
-    for (node in seq_along(desc)) {
-      if (html_name(desc[node]) == "ul")
-        d <- html_children(desc[node])
-      else if (html_name(desc[node]) == "ol")
-        d <- html_children(desc[node])
-      else
-        d <- desc[node]
-      if (node == 1)
-        descrip <- d
-      else
-        descrip <- append(descrip, d)
-    }
-    
-    attributes(descrip)$class <- attributes(desc)$class
-    
-    # description tabulation
-    tag <- sapply(descrip, html_name)
-    content <- sapply(descrip, html_text)
-    desc <- tibble(tag, content)
-    
-    # format text
-    desc <- desc %>% 
-      mutate(content = str_squish(content),
-             content = ifelse(str_detect(tag, "li"), 
-                              str_replace(content, "^(.+)$", "• \\1"),
-                              content),
-             content = ifelse((tag == "div" | tag == "p") & 
-                                str_count(content, "\\w+\\s") < 4 &
-                                !str_detect(content, "^-\\s") &
-                                !str_detect(content, "•\\s"),
-                              str_replace(content, "^(.+)$", "<strong>\\1</strong>"),
-                              content),
-             content = ifelse((tag == "div" | tag == "p") & nchar(content) < 50,
-                              str_replace(content, "^(.+)$", "<strong>\\1</strong>"),
-                              content),
-             content = ifelse(tag == "strong",
-                              str_replace(content, "^(.+)$", "<strong>\\1</strong>"),
-                              content),
-             content = str_replace(content, "^(.+)$", "\\1<br>"),
-             content = str_replace(content, "^<strong>", "<br><strong>"))
-    
-    # filter long paragraph
-    desc <- desc %>% 
-      filter(!((tag == "div" | tag == "p") & nchar(content) > 200))
-    
-    # clean format
-    topost_text <- desc$content %>% 
-      toString() %>% 
-      str_replace_all(",\\s,\\s", "<br>") %>% 
-      str_replace_all(">,\\s", ">") %>% 
-      str_replace_all("<br>-\\s", "<br>• ") %>% 
-      str_replace_all("(\\w+\\.?\\s?)<br>(\\w+)", "\\1<br><br>\\2") %>% 
-      str_replace_all("<br></strong>(<br>)?", "</strong><br><br>") %>% 
-      str_remove("^<br>") %>% 
-      str_remove("<br>$")
-    
-    if (n == num[[1]]) txt <- topost_text
-    else txt <- append(txt, topost_text)
-    
-  }
-  
+  } # end of loop
   return(txt)
-  
 }
