1232294afd5285b6cfc2ebf355737dee606176aa
Andi <getdata@duck.com>
2023-06-05 10:20:18 +0700

    Add function

--- audit.R
+++ audit.R
@@ -1,8 +1,13 @@
 cat("--------- Audit ----------\n")
 suppressPackageStartupMessages(source("requirement.R"))
+source("function/jobdescform.R")
+source("function/glints_desc.R")
 library(unmplymnt)
 library(purrr)
 library(telegram.bot)
+library(rvest)
+library(dplyr)
+library(stringr)
 
 # Keywords
 key_search <- c("audit kepatuhan", "compliance audit")
@@ -12,6 +17,7 @@ exclude_title <- list("tax", "manager", "senior")
 
 # Bot settings
 bot <- Bot(token = bot_token("IDNRBOT"))
+
 send_msg <- function(bot, msg) {
   map(msg, ~{
     tryCatch(
@@ -30,11 +36,41 @@ send_msg <- function(bot, msg) {
   })
 }
 
+post_job <- function(j, scraped) {
+  if (nrow(j) == 0) {
+    cat("No job to post today\n")
+  } else {
+    cat(
+      paste(
+        "Total", nrow(j), "job(s). Keywords:",
+        paste0(map_chr(keyword, ~.x), collapse = ", "), "\n"
+      )
+    )
+    # create message
+    msg <- map(seq_len(nrow(j)), ~{
+      paste0(
+        "*", toupper(j$job_title[.x]), "*\nat ", j$company[.x], "\n\n",
+        j$job_url[.x], "\n\nCategory: ", j$category,
+        "\nKeyword: #", keyword[[1]], " #", keyword[[2]]
+      )
+    })
+    # send message
+    cat("Sending message to Telegram...\n")
+    send_msg(bot, msg)
+    # save scraped details
+    j_scrape <- j[c("job_title", "company", "source", "job_id")]
+    names(j_scrape) <- c("job_title", "company_name", "source", "id")
+    j_scrape$timestamp <- format(Sys.time()) # "%Y-%m-%d %T%z"
+    j_scrape$timezone <- Sys.timezone()
+    scraped <- rbind(scraped, j_scrape)
+  }
+  write.csv(scraped, "output/audit.csv", row.names = FALSE)
+}
+
 # Load previous data
 scraped <- read.csv("output/audit.csv")
 
 # Jobstreet
-# pulling data
 cat("Get data from Jobstreet...\n")
 j <- tryCatch({
   suppressMessages(map_df(key_search, ~jobstreet(.x)))
@@ -42,6 +78,7 @@ j <- tryCatch({
 error = function(e) {
   cat("ERROR: ", conditionMessage(e), "\n")
 })
+
 j <- unique(j)
 j <- map_df(location, ~{
   j[grepl(.x, j$city), ]
@@ -68,37 +105,9 @@ j_keys[[(ncol(j_keys) + 1)]] <- as.logical(rowSums(j_keys))
 j_keys <- j_keys[[ncol(j_keys)]]
 j <- j[j_keys, ]
 
-if (nrow(j) == 0) {
-  cat("No job from Jobstreet to post today\n")
-} else {
-  cat(
-    paste0(
-      "Total ", nrow(j), " job(s). Keywords: ",
-      paste0(map_chr(keyword, ~.x), collapse = ", ")
-    )
-  )
-  # create message
-  msg <- map(seq_len(nrow(j)), ~{
-    paste0(
-      "*", toupper(j$job_title[.x]), "*\nat ", j$company[.x], "\n\n",
-      j$job_url[.x],
-      "\n\nCategory: ", j$category,
-      "\nKeyword: #", keyword[[1]], " #", keyword[[2]]
-    )
-  })
-  # send message
-  cat("Sending message to Telegram...\n")
-  send_msg(bot, msg)
-  # save scraped details
-  j_scrape <- j[c("job_title", "company", "source", "job_id")]
-  names(j_scrape) <- c("job_title", "company_name", "source", "id")
-  j_scrape$timestamp <- as.character(Sys.time())
-  j_scrape$timezone <- Sys.timezone()
-  scraped <- rbind(scraped, j_scrape)
-}
+post_job(j, scraped)
 
 # Glints
-# pulling data
 cat("Get data from Glints...\n")
 g <- tryCatch({
   suppressMessages(map_df(key_search, ~glints(.x)))
@@ -132,33 +141,4 @@ g_keys[[(ncol(g_keys) + 1)]] <- as.logical(rowSums(g_keys))
 g_keys <- g_keys[[ncol(g_keys)]]
 g <- g[g_keys, ]
 
-if (nrow(g) == 0) {
-  cat("No job from Glints to post today\n")
-} else {
-  cat(
-    paste0(
-      "Total ", nrow(g), " job(s). Keywords: ",
-      paste0(map_chr(keyword, ~.x), collapse = ", ")
-    )
-  )
-  # create message
-  msg <- map(seq_len(nrow(g)), ~{
-    paste0(
-      "*", toupper(g$job_title[.x]), "*\nat ", g$company[.x], "\n\n",
-      g$job_url[.x],
-      "\n\nCategory: ", g$category[.x],
-      "\nKeyword: #", keyword[[1]], " #", keyword[[2]]
-    )
-  })
-  # send message
-  cat("Sending message to Telegram...\n")
-  send_msg(bot, msg)
-  # save scraped details
-  g_scrape <- g[c("job_title", "company", "source", "job_id")]
-  names(g_scrape) <- c("job_title", "company_name", "source", "id")
-  g_scrape$timestamp <- as.character(Sys.time())
-  g_scrape$timezone <- Sys.timezone()
-  scraped <- rbind(scraped, g_scrape)
-}
-
-write.csv(scraped, "output/audit.csv", row.names = FALSE)
+post_job(g, scraped)
